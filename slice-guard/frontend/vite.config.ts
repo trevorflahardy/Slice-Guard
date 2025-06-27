b3928b3b11b78eb3b560fecf1a58f96b36077e94
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2025-06-27 16:41:01 -0400

    refactor: update Tailwind CSS integration and improve configuration for better styling support

--- slice-guard/frontend/vite.config.ts
+++ slice-guard/frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@shared': fileURLToPath(new URL('../shared', import.meta.url)),
